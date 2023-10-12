b9270ba81f32899572d2ed83a84ffdbf688f3c87
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-10-11 22:28:16 -0400

    added logic to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,7 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+console.log('initializing db')
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -13,9 +14,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Updating Database');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({
+    id: 1,
+    value: content
+  });
+  const result = await request;
+  console.log('Saved data to database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Reading database');
+  const db = await openDB('jate', 1);
+  console.log('opened db')
+  const tx = db.transaction('jate', 'readonly');
+  console.log('message', tx)
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  result ? console.log('Database content', result.value) : console.log("No Data");
+  return result?.value
+
+}
 
 initdb();
